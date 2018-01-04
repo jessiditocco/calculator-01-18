4b1eba6f8c12dbf4e2a80f515d651d113e0122f6
Hackbright Student <no-reply@hackbrightacademy.com>
2018-01-04 12:34:25 -0800

    changed variable name of sum stylized operators

--- arithmetic.py
+++ arithmetic.py
@@ -3,15 +3,15 @@
 
 def add(num1, num2):
     """Return the sum of the two inputs."""
-    sum = num1 + num2
-    return sum
+    the_sum = num1 + num2
+    return the_sum
 
 # print add(2,3)
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
-    difference = num1-num2
+    difference = num1 - num2
     return difference
 
 # print subtract(8,6)
@@ -19,7 +19,7 @@ def subtract(num1, num2):
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
-    product = num1*num2
+    product = num1 * num2
     return product
 
 # print multiply(8, 2)
@@ -27,7 +27,7 @@ def multiply(num1, num2):
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
-    quotient = num1/num2
+    quotient = num1 / num2
     return quotient
 
 # print divide(10, 2)
@@ -35,7 +35,7 @@ def divide(num1, num2):
 
 def square(num1):
     """Return the square of the input."""
-    square = num1**2
+    square = num1 ** 2
     return square
 
 # print square(9)
@@ -43,7 +43,7 @@ def square(num1):
 
 def cube(num1):
     """Return the cube of the input."""
-    cube = num1**3
+    cube = num1 ** 3
     return cube
 
 # print cube(2)
